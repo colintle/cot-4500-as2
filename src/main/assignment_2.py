ee4ada74e88a353fe4d5eab21d2f6729fe0f016e
Colin Le <Colin.Le@ucf.edu>
2024-02-21 22:32:55 -0500

    Question 2 and 3

--- src/main/assignment_2.py
+++ src/main/assignment_2.py
@@ -11,3 +11,47 @@ def neville(x, f_x, x_target):
             p[i][j] = ((x_target - x[i+j]) * p[i][j-1] + (x[i] - x_target) * p[i+1][j-1]) / (x[i] - x[i+j])
 
     return p[0][n-1]
+
+def divided_difference(xi, fi):
+    n = len(xi)
+    diff = np.zeros((n, n))
+
+    for i in range(n):
+        diff[i][0] = fi[i]
+
+    for i in range(1, n):
+        for j in range(1,i+1):
+            diff[i][j] = (diff[i][j-1] - diff[i-1][j - 1]) / (xi[i] - xi[i-j])
+
+    return diff
+
+def return_last(xi, diffs):
+    last= []
+    for i in range(len(xi)):
+        for j in range(i+1):
+            if j == i:
+                last.append(diffs[i][j])
+    return [last]
+
+def print_polynomial(xi, diffs, degree):
+    terms = [f"{diffs[0][0]:.6f}"]
+    for i in range(1, degree + 1):
+        coeff = diffs[i][i]
+        term = f"{coeff:+.6f}"
+        if (term[0] == "+"): 
+            term = term[1:]
+        for j in range(i):
+            term += f"*(x{-xi[j]:+.6f})"
+        terms.append(term)
+    polynomial = " + ".join(terms)
+    return polynomial
+
+def incremental_newton_polynomial(x, xi, diffs):
+    polynomial = diffs[0][0]
+    for i in range(1, len(xi)):
+        term = diffs[0][i]
+        for j in range(i):
+            term *= (x - xi[j])
+        polynomial += term
+    return polynomial
+
--- src/test/test_assignment_2.py
+++ src/test/test_assignment_2.py
@@ -1,4 +1,5 @@
-from src.main.assignment_2 import neville
+from src.main.assignment_2 import neville, divided_difference, return_last, print_polynomial, incremental_newton_polynomial
+import numpy as np
 
 #Question 1
 x = [3.6, 3.8, 3.9]
@@ -7,4 +8,31 @@ target = 3.7
 
 interpolated = neville(x, f_x, target)
 print(interpolated)
+print()
+
+#Question 2
+xi = [7.2, 7.4, 7.5, 7.6]
+fi = [23.5492, 25.3913, 26.8224, 27.4589]
+
+diffs = divided_difference(xi, fi)
+polynomial_degree_1 = print_polynomial(xi, diffs, 1)
+polynomial_degree_2 = print_polynomial(xi, diffs, 2)
+polynomial_degree_3 = print_polynomial(xi, diffs, 3)
+
+print(f"Polynomial of degree 1: P_1(x) = {polynomial_degree_1}")
+print(f"Polynomial of degree 2: P_2(x) = {polynomial_degree_2}")
+print(f"Polynomial of degree 3: P_3(x) = {polynomial_degree_3}")
+print()
+
+#Question 3
+last = return_last(xi, diffs)
+x_value = 7.3
+
+p1 = incremental_newton_polynomial(x_value, xi[:2], [diff[:2] for diff in last])
+p2 = incremental_newton_polynomial(x_value, xi[:3], [diff[:3] for diff in last])
+p3 = incremental_newton_polynomial(x_value, xi[:4], [diff[:4] for diff in last])
+print(p1)
+print(p2)
+print(p3)
+
 
